57d84d72ffee8e412067422e85558970f87cbc04
エッケザックス <22173829+kagekiyo7@users.noreply.github.com>
2024-04-11 19:59:28 +0900

    Update extract_mld.py

--- extract_mld.py
+++ extract_mld.py
@@ -15,6 +15,9 @@ def extract_mld(binary: bytes):
             break
         size = int.from_bytes(binary[file_i+4:file_i+8]) + 8
         mld_binary = binary[file_i:file_i+size]
+        if not get_mld_metadata.is_valid_mld(mld_binary): 
+            file_i += 1
+            continue
         mld_dict["binary"] = mld_binary
         mld_dict["title"] = get_mld_metadata.get_metadata(mld_binary).get("title", None)
         file_i += 1
@@ -29,19 +32,20 @@ def main(file_path):
     mlds = extract_mld(target_binary)
 
     def output(files, ext):
-        i = 0
-        for file_dict in files:
+        for i, file_dict in enumerate(files):
+            dig = len(str(len(files)))
+            i = str(i).zfill(2) if dig > 2 else str(i).zfill(dig)
             if title := file_dict.get("title", False):
                 output_path = os.path.join(dir_name, f"{str(i).zfill(2)} {title}.{ext}")
-                with open(output_path, "wb") as f:
-                    f.write(file_dict["binary"])
-                    print(f"{os.path.basename(output_path)}: done!")
             else:
                 output_path = os.path.join(dir_name, f"{file_name}_{i}.{ext}")
-                with open(output_path, "wb") as f:
-                    f.write(file_dict["binary"])
-                    print(f"{os.path.basename(output_path)}: done!")
-            i += 1
+            with open(output_path, "wb") as f:
+                f.write(file_dict["binary"])
+                print(f"{os.path.basename(output_path)}: done!")
+
+    if not mlds:
+        print(f"{os.path.basename(file_path)}: There is no mld.")
+        return
     output(mlds, ext="mld")
 
 if __name__ == "__main__":
